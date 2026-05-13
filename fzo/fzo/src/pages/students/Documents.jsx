ed4e4ef5d4f3296c66065701d9d0cfcb17121123
nikit <nikitos-ooo@main.ru>
2026-05-13 10:35:52 +0500

    dociments front fix

--- fzo/fzo/src/pages/students/Documents.jsx
+++ fzo/fzo/src/pages/students/Documents.jsx
@@ -20,17 +20,18 @@ function Documents() {
     return (
         <div className="docs-page">
             <div className="docs-card">
-                <h1>Документы для студентов</h1>
 
+                {/* Инфо-блок с заголовком внутри */}
                 <div className="docs-info-block">
+                    <h1>Документы для студентов</h1>
+
                     <h2>Основные права и обязанности обучающихся</h2>
 
-                    <p>
-                        <b>Перевод:</b>
-                    </p>
+                    <p><b>Перевод:</b></p>
                     <ol>
                         <li>
-                            Студентов в УГНТУ из других вузов осуществляется: <b>с 25 июня по 07 июля</b>
+                            Студентов в УГНТУ из других вузов осуществляется:{" "}
+                            <b>с 25 июня по 07 июля</b>
                         </li>
                         <li>
                             Студентов УГНТУ с одной специальности на другую осуществляется:{" "}
@@ -57,7 +58,8 @@ function Documents() {
 
                     <h2>Восстановление ранее отчисленных студентов</h2>
                     <p>
-                        Производится: <b>с 25 января по 15 февраля</b>; <b>с 20 августа по 10 сентября</b>
+                        Производится: <b>с 25 января по 15 февраля</b>;{" "}
+                        <b>с 20 августа по 10 сентября</b>
                     </p>
                     <p>
                         Если Вы отчислены из УГНТУ, но хотите и можете продолжить обучение на
@@ -66,47 +68,49 @@ function Documents() {
                             здесь
                         </a>
                     </p>
-
-                    <p className="docs-info-link">
-                        <b>Ссылки: </b>
-                        <a href="https://www.rusoil.net/sveden/document" target="_blank" rel="noreferrer">
-                            https://www.rusoil.net/sveden/document
-                        </a>
-                    </p>
                 </div>
 
+                {/* Список документов */}
                 <div className="docs-list">
                     {docs.length === 0 && (
                         <p style={{ color: "#555", textAlign: "center" }}>Документы не найдены</p>
                     )}
                     {docs.map(doc => (
-                        <div key={doc.id} className={`doc-item ${openId === doc.id ? "doc-item--open" : ""}`}>
+                        <div
+                            key={doc.id}
+                            className={`doc-item ${openId === doc.id ? "doc-item--open" : ""}`}
+                        >
                             <button className="doc-toggle" onClick={() => toggle(doc.id)}>
                                 <span>{doc.title}</span>
-                                <span className="doc-arrow">{openId === doc.id ? "▲" : "▼"}</span>
+                                <span className="doc-arrow">▼</span>
                             </button>
-                            {openId === doc.id && (
-                                <div className="doc-body">
-                                    {doc.description && (
-                                        <div
-                                            className="doc-description ql-editor"
-                                            dangerouslySetInnerHTML={{ __html: doc.description }}
-                                        />
-                                    )}
-                                    {doc.fileName && (
-                                        
-                                        <a    href={`/api/documents/${doc.id}/file`}
-                                            className="doc-download"
-                                            download
-                                        >
-                                            📄 Скачать: {doc.fileName}
-                                        </a>
-                                    )}
+
+                            {/* Обёртка для плавной анимации */}
+                            <div className="doc-body-wrapper">
+                                <div className="doc-body-inner">
+                                    <div className="doc-body">
+                                        {doc.description && (
+                                            <div
+                                                className="doc-description ql-editor"
+                                                dangerouslySetInnerHTML={{ __html: doc.description }}
+                                            />
+                                        )}
+                                        {doc.fileName && (
+                                            
+                                            <a    href={`/api/documents/${doc.id}/file`}
+                                                className="doc-download"
+                                                download
+                                            >
+                                                📄 Скачать: {doc.fileName}
+                                            </a>
+                                        )}
+                                    </div>
                                 </div>
-                            )}
+                            </div>
                         </div>
                     ))}
                 </div>
+
             </div>
         </div>
     );
